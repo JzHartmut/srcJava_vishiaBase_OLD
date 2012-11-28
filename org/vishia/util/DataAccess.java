667e9b3f3628c8d72409a0195b2d763e0827f4a5
Hartmut Schorrig <hartmut@vishia.org>
2012-11-29 00:00:00 +0100

    Zmake:Improve

--- org/vishia/util/DataAccess.java
+++ org/vishia/util/DataAccess.java
@@ -1,6 +1,8 @@
 package org.vishia.util;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,8 +13,9 @@ import java.util.Map;
  *
  */
 public class DataAccess {
-  /**Version and history
+  /**Version, history and license.
    * <ul>
+   * <li>2012-11-24 new: {@link DatapathElement} for describing more complex path for access.
    * <li>2012-11-18 new: {@link #setBit(int, int, boolean)} as little universal routine.
    * <li>2012-11-16 new: {@link #getInt(Object)}, {@link #getFloat(Object)} from {@link ObjectValue}, last one is deprecated now.
    * <li>2012-11-04 chg: parameter bContainer in getData(...): Sometimes a container is ispected
@@ -52,26 +55,29 @@ public class DataAccess {
 
   /**Reads content from the data.
    * <ul>
-   * <li>The namedDataPool provides additional data, which are addressed by the first part of path.
-   *    The the data from this pool are used instead dataPool. Elsewhere the first part of the path should be 
-   *    able to found in the start instance of dataPool.
+   * <li>The namedDataPool provides additional data, which may be addressed by the first part of path if it starts
+   *   with a "$" (a variable). Then the data from this pool are used instead dataPool. 
+   *   Elsewhere the first part of the path should be able to found in the start instance of dataPool.
    *  <li>If any data object found inside the path is instanceof Map and the key is a String, then the sub object
    *    is gotten from the map with the next part of the path used as the key.
+   *  <li>If any data object is instanceof {@link TreeNodeBase} and the field identifier is not found in this instance,
+   *    a child node with the given name is searched. The TreeNodeBase is the super class of {@link org.vishia.xmlSimple.XmlNodeSimple}
+   *    which is used to present a ZBNF parse result. Therewith the {@link org.vishia.zbnf.ZbnfParser#getResultTree()}
+   *    can be used as data input. The tag names of that result tree follow the semantic in the string given Syntax script.
+   *  <li>Nevertheless any Java data objects can be used as dataPool. It is independent of that special possibilities.   
    *  <li>methods with constant string or numeric parameters are admissible as part of the path (TODO)  
    * </ul>
    * @param path The path, elements in any list element.
    * @param dataPool the object where the path starts from.
    * @param namedDataPool variables valid for the current block
-   * @param noException returns an info string starting with "<?" if there is any error.
    * @param accessPrivate if true then private data are accessed too. The accessing of private data may be helpfull
    *  for debugging. It is not recommended for general purpose! The access mechanism is given with 
    *  {@link java.lang.reflect.Field#setAccessible(boolean)}.
-   *  @param bContainer If the element is a container, returns it. Elsewhere build a List
+   * @param bContainer If the element is a container, returns it. Elsewhere build a List
    *    to return a container for iteration. A container is any object implementing java.util.Map or java.util.Iterable
    * @return Any data object addressed by the path.
    * @throws IllegalArgumentException
    */
-  @Deprecated
   public static Object getData(
       List<DatapathElement> path
       , Object dataPool
@@ -103,13 +109,18 @@ public class DataAccess {
         else {
           try{ 
             clazz1 = data1.getClass();
-            Field field = clazz1.getDeclaredField(element.ident);
-            field.setAccessible(accessPrivate);
-            try{ data1 = field.get(data1);
-            
-            } catch(IllegalAccessException exc){
-              //try special types:
-              throw new NoSuchFieldException(element.ident + " in " + path); 
+            if(element.fn){
+              Method method = clazz1.getDeclaredMethod(element.ident);
+              data1 = method.invoke(data1);
+            } else {
+              Field field = clazz1.getDeclaredField(element.ident);
+              field.setAccessible(accessPrivate);
+              try{ data1 = field.get(data1);
+              
+              } catch(IllegalAccessException exc){
+                //try special types:
+                throw new NoSuchFieldException(element.ident + " in " + path); 
+              }
             }
             element = iter.hasNext() ? iter.next() : null;
           } catch(NoSuchFieldException exc){
@@ -128,6 +139,21 @@ public class DataAccess {
             } else {
               throw new NoSuchFieldException(element.ident); 
             }
+          } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
           }
         }
       }
@@ -204,10 +230,47 @@ public class DataAccess {
 
   
   
+  /**Class holds one element for access to data.
+   * Instances of this class can be created using {@link org.vishia.zbnf.ZbnfJavaOutput} to fill from a parse result.
+   * Therefore some methods have a special naming which matches to the semantic of the used parser script.
+   */
   public static final class DatapathElement
   {
+    /**Name of the element or method in any instance.
+     * From Zbnf <$?ident>
+     */
     public String ident;
+
+    /**True if it is a method.  From Zbnf <$?fn> */
     public boolean fn;
+    
+    /**List of arguments of a method. If null, the method has not arguments. */
+    private List<Object> fnArgs;
+    
+    /**Set a textual argument of a access method. From Zbnf <""?textArg>. */
+    public void set_textArg(String arg){ addToList(arg); }
+    
+    /**Set a integer (long) argument of a access method. From Zbnf <#?intArg>. */
+    public void set_intArg(long arg){ addToList(arg); }
+    
+    /**Set a float (double) argument of a access method. From Zbnf <#f?floatArg>. */
+    public void set_floatArg(double arg){ addToList(arg); }
+    
+    /**Adds any argument. This method is called from {@link #set_floatArg(double)} etc. */
+    public void addToList(Object arg){
+      if(fnArgs == null){
+        fnArgs = new LinkedList<Object>();
+      }
+      fnArgs.add(arg);
+    }
+    
+    /**For debugging.*/
+    @Override public String toString(){
+      if(!fn){ return ident;}
+      else{
+        return ident + "(...)";
+      }
+    }
   }
 
   
