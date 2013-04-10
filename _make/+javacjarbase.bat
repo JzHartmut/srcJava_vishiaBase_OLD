85e860b984321c92ad9fe1b8a3b5c1c3a54d1947
Hartmut Schorrig <hartmut@vishia.org>
2013-04-11 00:00:00 +0200

    * +javacjarbase.bat: possible location for jdk enhanced

--- _make/+javacjarbase.bat
+++ _make/+javacjarbase.bat
@@ -27,6 +27,8 @@ REM  The java-copiler may be located at a user-specified position.
 REM  Set the environment variable JAVA_HOME, where bin/javac will be found.
 set JAVA_JDK=D:\Progs\JAVA\jdk1.6.0_21
 if exist "%JAVA_JDK%" goto :JavaOk
+set JAVA_JDK=D:\Programme\JAVA\jdk1.6.0_21
+if exist "%JAVA_JDK%" goto :JavaOk
 set JAVA_JDK=C:\Progs\JAVA\jdk1.6.0_21
 if exist "%JAVA_JDK%" goto :JavaOk
 set JAVA_JDK=D:\Progs\JAVA\jdk1.6.0_21
