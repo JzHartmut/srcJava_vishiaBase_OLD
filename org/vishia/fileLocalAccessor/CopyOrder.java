e370cb081f1887fb1529260bf1ce03c2121409c4
Hartmut Schorrig <hartmut@vishia.org>
2013-04-21 00:00:00 +0200

    * CopyOrder.java

---
+++ org/vishia/fileLocalAccessor/CopyOrder.java
@@ -0,0 +1,38 @@
+package org.vishia.fileLocalAccessor;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.vishia.util.FileRemote;
+
+
+
+/**Order for copy, created on check.
+ * @author Hartmut Schorrig
+ *
+ */
+class CopyOrder
+{
+
+  final long ident;
+  
+  /**If the order is to old, remove it. */
+  long dateCreation;
+  
+  /**File or dir to copy. */
+  FileRemote fileSrc, fileDst;
+  
+  
+  /**List of files to handle between {@link #checkCopy(org.vishia.util.FileRemoteAccessor.FileRemote.CallbackEvent)}
+   * and {@link #execCopy(org.vishia.util.FileRemoteAccessor.FileRemote.CallbackEvent)}. */
+  final List<File> listCopyFiles = new LinkedList<File>();
+  
+
+  
+  CopyOrder(long ident){
+    this.ident = ident;
+  }
+  
+  
+}
