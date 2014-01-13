2ad6466b3c1959eb22eca70d84a184ca5f12f2ea
Hartmut Schorrig <hartmut@vishia.org>
2014-01-14 00:00:00 +0100

    * add StringPartAppend

---
+++ org/vishia/util/StringPartAppend.java
@@ -0,0 +1,90 @@
+package org.vishia.util;
+
+import java.io.IOException;
+
+/**This class combines an Appendable with the capability of StringPartScan.
+ * All append methods sets the endMax of the StringPart to the new length. The current part end
+ * will be set to endMax only if is on endMax before append.
+ * @author hartmut Schorrig
+ *
+ */
+public class StringPartAppend extends StringPartScan implements Appendable
+{
+  
+  /**Version, history and license.
+   * <ul>
+   * <li>2014-01-13 Hartmut created: It is the idea to combine the StringPart to analyze Strings
+   *   with an Appendable, especially a StringBuilder.   
+   * </ul>
+   * 
+   * <b>Copyright/Copyleft</b>:
+   * For this source the LGPL Lesser General Public License,
+   * published by the Free Software Foundation is valid.
+   * It means:
+   * <ol>
+   * <li> You can use this source without any restriction for any desired purpose.
+   * <li> You can redistribute copies of this source to everybody.
+   * <li> Every user of this source, also the user of redistribute copies
+   *    with or without payment, must accept this license for further using.
+   * <li> But the LPGL is not appropriate for a whole software product,
+   *    if this source is only a part of them. It means, the user
+   *    must publish this part of source,
+   *    but don't need to publish the whole source of the own product.
+   * <li> You can study and modify (improve) this source
+   *    for own using or for redistribution, but you have to license the
+   *    modified sources likewise under this LGPL Lesser General Public License.
+   *    You mustn't delete this Copyright/Copyleft inscription in this source file.
+   * </ol>
+   * If you are intent to use this sources without publishing its usage, you can get
+   * a second license subscribing a special contract with the author. 
+   * 
+   * @author Hartmut Schorrig = hartmut.schorrig@vishia.de
+   * 
+   * 
+   */
+  //@SuppressWarnings("hiding")
+  static final public String sVersion = "2014-01-12";
+
+  
+  public StringPartAppend(){
+    super(new StringBuilder(1000));
+  }
+
+  
+  public void clear(){ ((StringBuilder)content).setLength(0); }
+
+  @Override public Appendable append(CharSequence csq) throws IOException
+  { ((StringBuilder)content).append(csq);
+    if(end == endMax){
+      end = endMax = content.length();
+    } else {
+      endMax = content.length();
+    }
+    return this;
+  }
+
+  @Override public Appendable append(char c) throws IOException
+  { ((StringBuilder)content).append(c);
+    if(end == endMax){
+      end = endMax = content.length();
+    } else {
+      endMax = content.length();
+    }
+    return this;
+  }
+
+  @Override public Appendable append(CharSequence csq, int from, int to) throws IOException
+  { ((StringBuilder)content).append(csq, from, to);
+    if(end == endMax){
+      end = endMax = content.length();
+    } else {
+      endMax = content.length();
+    }
+    return this;
+  }
+
+  
+  StringBuilder buffer(){ return (StringBuilder)content; }
+  
+  
+}
